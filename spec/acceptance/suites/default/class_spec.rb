cfe7acc8d0bd71127d9442323442f0b571ea4296
Chris Tessmer <chris.tessmer@onyxpoint.com>
2017-04-15 01:53:20 -0400

    PKI acceptance tests

--- spec/acceptance/suites/default/class_spec.rb
+++ spec/acceptance/suites/default/class_spec.rb
@@ -3,23 +3,24 @@ require 'spec_helper_acceptance'
 test_name 'simp_gitlab class'
 
 describe 'simp_gitlab class' do
+
   let(:server) {only_host_with_role( hosts, 'server' )}
-  let(:curl_ssl_cmd ) {
-    server = only_host_with_role( hosts, 'server' )
-    fqdn = fact_on(server, 'fqdn')
-    'curl  --cacert /etc/pki/simp/x509/cacerts/cacerts.pem' +
-         " --cert /etc/pki/simp/x509/public/#{fqdn}.pub" +
-         " --key /etc/pki/simp/x509/private/#{fqdn}.pem"
-  }
   let(:pupenv) {{'PUPPET_EXTRA_OPTS' => '--logdest /var/log/puppetlabs/puppet/beaker.log'}}
-  let(:manifest) {
+  let(:manifest) do
     <<-EOS
       class { 'simp_gitlab':
         pki => true,
         app_pki_external_source => '/etc/pki/simp-testing/pki',
       }
     EOS
-  }
+  end
+  let(:curl_ssl_cmd ) do
+    server = only_host_with_role( hosts, 'server' )
+    fqdn   = fact_on(server, 'fqdn')
+    'curl  --cacert /etc/pki/simp_apps/gitlab/x509/cacerts/cacerts.pem' +
+         " --cert /etc/pki/simp_apps/gitlab/x509/public/#{fqdn}.pub" +
+         " --key /etc/pki/simp_apps/gitlab/x509/private/#{fqdn}.pem"
+  end
 
   context 'default parameters (PKI enabled)' do
     # Using puppet_apply as a helper
@@ -31,13 +32,11 @@ describe 'simp_gitlab class' do
       apply_manifest_on(server, manifest, :catch_changes => true, :environment => pupenv)
     end
 
-    it 'allows http connection on port 80' do
+    it 'allows https connection on port 443' do
       shell 'sleep 90' # give it some time to start up
       fqdn = fact_on(server, 'fqdn')
-      describe command( "#{curl_ssl_cmd} -L https://#{fqdn}/users/sign_in" ) do
-        its(:stdout) { should match /GitLab|password/ }
-      end
+      result = on(server, "#{curl_ssl_cmd} -L https://#{fqdn}/users/sign_in" )
+      expect(result.stdout).to match(/GitLab|password/)
     end
-
   end
 end
