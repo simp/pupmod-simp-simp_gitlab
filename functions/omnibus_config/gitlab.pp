998053188d2e35d759f1fafc6304fe2ae0b423a9
lnemsick-simp <lnemsick.simp@gmail.com>
2019-03-18 12:49:20 -0400

    (SIMP-6238) Update to puppet-gitlab v3.0.2
    - Switched from vshn/gitlab 1.13.3 to its replacement puppet/gitlab
      3.0.2 and reworked the manifests, as appropriate, to work with the
      newer module.
    - Added manage_package and package_ensure parameters to simp_gitlab
    
    SIMP-6284 #close
    SIMP-6285 #close
    SIMP-6238 #close

--- functions/omnibus_config/gitlab.pp
+++ functions/omnibus_config/gitlab.pp
@@ -1,5 +1,5 @@
 # Compile a hash of settings for the gitlab module's parameters, using SIMP settings
-# @return Hash of `vshn/gitlab` parameters
+# @return Hash of `puppet/gitlab` parameters
 function simp_gitlab::omnibus_config::gitlab() {
 
   # For HTTPS, non-standard ports *must* be included in the external_url:
@@ -9,18 +9,22 @@ function simp_gitlab::omnibus_config::gitlab() {
   }
 
   $_gitlab_default_parameters = {
-    'external_url'      => $_external_url,
-    'external_port'     => $simp_gitlab::tcp_listen_port,
-    'nginx'             => simp_gitlab::omnibus_config::nginx(),
-    'gitlab_rails'      => simp_gitlab::omnibus_config::gitlab_rails(),
-    'shell'             => simp_gitlab::omnibus_config::gitlab_shell(),
-    'mattermost'        => simp_gitlab::omnibus_config::mattermost(),
-    'mattermost_nginx'  => { 'enable' => false },
-    'prometheus'        => { 'enable' => false },
-    'node_exporter'     => { 'enable' => false },
-    'redis_exporter'    => { 'enable' => false },
-    'postgres_exporter' => { 'enable' => false },
-    'gitlab_monitor'    => { 'enable' => false },
+    # If simp_gitlab is managing the GitLab package, disable that capability in
+    # the gitlab class
+    'manage_package'          => !$simp_gitlab::manage_package,
+    'manage_upstream_edition' => $simp_gitlab::edition,
+    'external_url'            => $_external_url,
+    'external_port'           => $simp_gitlab::tcp_listen_port,
+    'nginx'                   => simp_gitlab::omnibus_config::nginx(),
+    'gitlab_rails'            => simp_gitlab::omnibus_config::gitlab_rails(),
+    'shell'                   => simp_gitlab::omnibus_config::gitlab_shell(),
+    'mattermost'              => simp_gitlab::omnibus_config::mattermost(),
+    'mattermost_nginx'        => { 'enable' => false },
+    'prometheus'              => { 'enable' => false },
+    'node_exporter'           => { 'enable' => false },
+    'redis_exporter'          => { 'enable' => false },
+    'postgres_exporter'       => { 'enable' => false },
+    'gitlab_monitor'          => { 'enable' => false },
     #'mattermost_nginx_eq_nginx' => true,
   }
 }
